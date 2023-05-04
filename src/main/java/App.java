48c62d4c5bfe90284261523b26efb62e61516297
CHRISHLOH <john.johnson127721@gmail.com>
2023-05-04 16:22:08 +0300

    2

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,5 +5,15 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
+        HelloWorld bean =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean.getMessage());
+        HelloWorld bean1 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+
+        Cat catBean = (Cat) applicationContext.getBean("cat");
+        Cat catBean1 = (Cat) applicationContext.getBean("cat");
+        System.out.println(bean == bean1);
+        System.out.println(catBean == catBean1);
     }
 }
